c79c8754b382b6eaa359dfa122901193917b1877
govindkrishna03 <govindkrishna28@gmail.com>
2025-04-18 14:12:31 +0530

    fixed error

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <body oncontextmenu="return false;"><Navbar />{children}</body>
+        <body><Navbar />{children}</body>
       </html>
     )
   }
